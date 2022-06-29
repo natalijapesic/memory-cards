13670e2fbb32fd495f3946aa2c3713c560e2e28e
natalija <natalijapesic@elfak.rs>
2022-06-29 17:08:34 +0200

    mixin to manage breakpoints

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthenticationService, User } from '../users';
 
 @Component({
@@ -8,12 +8,25 @@ import { AuthenticationService, User } from '../users';
 })
 export class NavBarComponent implements OnInit {
   user: User | null;
+  public screenWidth: number;
+
   constructor(private authenticationService: AuthenticationService) {
     this.user = null;
+    this.screenWidth = 0;
   }
 
   ngOnInit(): void {
     this.authenticationService.user.subscribe((x) => (this.user = x));
+    this.screenWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  isMobile(): boolean {
+    return this.screenWidth < 768 ? true : false;
   }
 
   onSignOut() {
