fa82b90760b11e09e005ee80250b65857a7bb22f
natalijapesic <natalijapesic@elfak.com>
2022-07-04 16:06:00 +0200

    add api

---
+++ memory-cards-api/src/cloud/functions.ts
@@ -0,0 +1,14 @@
+
+
+Parse.Cloud.define("hello", (req) => {
+  return "Hi";
+});
+
+Parse.Cloud.define("asyncFunction", async (req) => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return "Hi async";
+});
+
+Parse.Cloud.beforeSave("Test", () => {
+  throw new Parse.Error(9001, "Saving test objects is not available.");
+});
---
+++ memory-cards-api/src/index.ts
@@ -0,0 +1,50 @@
+import express from "express";
+
+const ParseServer = require("parse-server").ParseServer;
+
+const config = {
+  //databaseURI: "mongodb://localhost:27017",
+  databaseURI: "mongodb://mongoadmin:nignite@localhost:27017",
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + "/cloud/main.js",
+  appId: process.env.APP_ID || "myAppId",
+  masterKey: process.env.MASTER_KEY || "", //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || "http://localhost:1337/parse", // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Posts", "Comments"], // List of classes to support for query subscriptions
+  },
+};
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
+
+const app = express();
+
+// Serve the Parse API on the /parse URL prefix
+const mountPath = process.env.PARSE_MOUNT || "/parse";
+
+const api = new ParseServer(config);
+app.use(mountPath, api);
+
+// Parse Server plays nicely with the rest of your web routes
+app.get("/", function (req, res) {
+  res
+    .status(200)
+    .send(
+      "I dream of being a website.  Please star the parse-server repo on GitHub!"
+    );
+});
+
+const port = process.env.PORT || 1337;
+
+const httpServer = require("http").createServer(app);
+httpServer.listen(port, function () {
+  console.log("parse-server-example running on port " + port + ".");
+});
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
+
+module.exports = {
+  app,
+  config,
+};
+1
